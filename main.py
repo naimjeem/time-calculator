7da424637b73e6771ece5932b453c68688841205
Naim Jeem <naim36208@gmail.com>
2021-03-04 03:16:36 +0600

    project completed

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 # This entrypoint file to be used in development. Start by reading README.md
 from time_calculator import add_time
-from unittest import main
+# from unittest import main
 
 
-print(add_time("11:06 PM", "2:02"))
+print(add_time("11:43 PM", "24:20", "tuesday"))
 
 
 # Run unit tests automatically
-main(module='test_module', exit=False)
+# main(module='test_module', exit=False)
--- time_calculator.py
+++ time_calculator.py
@@ -1,7 +1,45 @@
-def add_time(start, duration):
+import datetime
 
+def add_time(start, duration, weekday_name = ' '):
 
+    start = datetime.datetime.strptime(start,'%I:%M %p')
+    h_duration,m_duration = duration.split(':')
+    t_duration = int(h_duration)*60 + int(m_duration)
+    calc_time = start + datetime.timedelta(minutes=int(t_duration))
+    day = calc_time.day
+    time = calc_time.strftime('%I:%M %p')
 
+    if day == 1:
+        day_text = ''
+    elif day == 2:
+        day_text = '(next day)'
+    else: 
+        day_text = '('+ str(day-1) + ' days later)'
 
+    list_weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    if weekday_name != ' ' and day >= 2:
+        weekday_name = weekday_name.lower().capitalize()
+        weekday_name_index = list_weekdays.index(weekday_name) 
+        i=1
+        indexw = weekday_name_index
+        while i != day:
+            if indexw == 6:
+                indexw = 0
+                i+=1
+            else:
+                indexw = indexw + 1
+                i+=1
 
-    return new_time
+        new_weekday_name =  list_weekdays[indexw]
+        new_time = str(time + ', ' + new_weekday_name + ' ' + day_text)
+    elif weekday_name != ' ' and day == 1:
+        new_time = str(time + ', '+ weekday_name.lower().capitalize())
+    else:
+        new_time = str(time + ' ' + day_text)
+
+    if new_time[0] == '0':
+        new_time = new_time[1:]
+    else:
+        new_time = new_time
+
+    return new_time.strip()
